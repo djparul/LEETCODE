0a468d1dd14a627f04704ee9d989e1e7ae95fbef
djparul <dparulsharma18@gmail.com>
2022-06-05 23:21:20 +0530

    plus one

---
+++ java/66.PlusOne.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        // Case 1 All are 9                
+        int arr[];
+        boolean flag=true; 
+         for(int i=0;i<digits.length;i++) {
+             if(digits[i]!=9) {
+                 flag=false;
+                 break;
+             }
+         }
+         if(flag == true) {
+             arr=new int[digits.length+1];
+             Arrays.fill(arr,0);
+             arr[0]=1;
+             return arr;
+         } else {  // Case 2 Last digit is 9
+                if(digits[digits.length-1] == 9) {
+                int carry = 1;   
+                int i = digits.length-1;
+                while(i >= 0) {
+                    int sum = carry+digits[i];
+                    digits[i] = sum % 10;
+                    carry = sum / 10;
+                    if(carry == 0) {
+                        break;
+                    }
+                    i--;
+                }
+               } else {             //Case 3: Last Digit not 9
+                   digits[digits.length-1]++;
+               }
+         }
+         return digits;
+    }
+}
