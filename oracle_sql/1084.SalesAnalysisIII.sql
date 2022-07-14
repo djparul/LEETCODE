cfe2786a8cb9f0f638a2e9cdbe96e87fda6f0ba6
djparul <106873522+djparul@users.noreply.github.com>
2022-07-14 16:30:55 -0700

    Update 1084.SalesAnalysisIII.sql

--- oracle_sql/1084.SalesAnalysisIII.sql
+++ oracle_sql/1084.SalesAnalysisIII.sql
@@ -1,6 +1,9 @@
 # https://leetcode.com/problems/sales-analysis-iii/
-select p.product_id, p.product_name 
-from product p where product_id not in
+select distinct(p.product_id), p.product_name 
+from product p
+inner join sales s
+on s.product_id = p.product_id
+where p.product_id not in
     ( select  s.product_id
      from sales s
      where s.sale_date not between '2019-01-01' and '2019-03-31')
